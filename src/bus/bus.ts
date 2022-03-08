b27441012cc95bee6c4c354b6c0e40c7572dea7e
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-03-08 13:13:47 +0100

    🏷️ Fixing missing option in Vue constructor

--- src/bus/bus.ts
+++ src/bus/bus.ts
@@ -4,8 +4,15 @@ import install from "./install";
 const bus = { EventBus, install };
 
 declare module "vue/types/vue" {
-  interface VueConstructor {
-    $bus: EventBus;
+  interface Vue {
+    readonly $bus: EventBus;
+  }
+}
+
+declare module "vue/types/options" {
+  // @ts-ignore
+  interface ComponentOptions<V extends Vue> { // eslint-disable-line
+    bus?: EventBus;
   }
 }
 
