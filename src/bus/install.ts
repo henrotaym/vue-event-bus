d8c7f33c2cdda6e8a76b684ca81017d0cd9579a2
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-03-08 12:27:51 +0100

    🐛 Using install method correctly.

--- src/bus/install.ts
+++ src/bus/install.ts
@@ -1,5 +1,21 @@
 import { PluginFunction } from "vue";
+import EventBus from "./EventBus";
 
-const install: PluginFunction<undefined> = () => {};
+const install: PluginFunction<undefined> = (Vue) => {
+  Vue.mixin({
+    beforeCreate() {
+      const options: any = this.$options;
+      if (!(options.bus instanceof EventBus)) return;
+      /** Setting $bus on instance. */
+      const bus = options.bus as EventBus;
+      Object.defineProperty(this, "$bus", {
+        /** Getting bus dynamically. */
+        get() {
+          return bus;
+        },
+      });
+    },
+  });
+};
 
 export default install;
